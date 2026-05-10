0469f272a9134589079aa0f562320913bdeb9daf
KAMRON-DEVELOPER <atajanovkamronbek2003@gmail.com>
2026-05-10 23:40:47 +0500

    minor fixes

--- stow/nvim/.config/nvim/lua/config/pack/extra/indent_blankline.lua
+++ stow/nvim/.config/nvim/lua/config/pack/extra/indent_blankline.lua
@@ -2,6 +2,16 @@ vim.pack.add({
   { src = "https://github.com/lukas-reineke/indent-blankline.nvim" },
 })
 
-require("ibl").setup({
-  enable = false,
+require("ibl").setup({ enabled = false })
+
+local map = vim.keymap.set
+
+map("n", "<leader>ui", "<cmd>IBLToggle<cr>", {
+  silent = true,
+  desc = "Toggle indent guides",
+})
+
+map("n", "<leader>uI", "<cmd>IBLToggleScope<cr>", {
+  silent = true,
+  desc = "Toggle indent scope",
 })
