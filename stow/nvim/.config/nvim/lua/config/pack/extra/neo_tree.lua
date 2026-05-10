96a58076b78cc7396d22d98650e25b5e84f5a8c1
KAMRON-DEVELOPER <atajanovkamronbek2003@gmail.com>
2026-05-10 23:43:36 +0500

    minor fixes

--- stow/nvim/.config/nvim/lua/config/pack/extra/neo_tree.lua
+++ stow/nvim/.config/nvim/lua/config/pack/extra/neo_tree.lua
@@ -1,5 +1,5 @@
 vim.pack.add({
-  { src = "https://github.com/nvim-lua/plenary.nvimm" },
+  { src = "https://github.com/nvim-lua/plenary.nvim" },
   { src = "https://github.com/MunifTanjim/nui.nvim" },
   { src = "https://github.com/nvim-tree/nvim-web-devicons" },
   {
